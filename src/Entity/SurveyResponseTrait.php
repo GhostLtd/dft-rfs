d70002274001c515053b8dbdfb8d0c532855b900
Mark Allman <mark@tomarq.co.uk>
2020-11-06 11:00:56 +0000

    Add survey contact fields to SurveyResponse

--- src/Entity/SurveyResponseTrait.php
+++ src/Entity/SurveyResponseTrait.php
@@ -18,6 +18,22 @@ trait SurveyResponseTrait
      */
     private $businessNature;
 
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contactName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contactTelephone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contactEmail;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -31,6 +47,43 @@ trait SurveyResponseTrait
     public function setBusinessNature(string $businessNature): self
     {
         $this->businessNature = $businessNature;
+
+        return $this;
+    }
+
+
+    public function getContactName(): ?string
+    {
+        return $this->contactName;
+    }
+
+    public function setContactName(string $contactName): self
+    {
+        $this->contactName = $contactName;
+
+        return $this;
+    }
+
+    public function getContactTelephone(): ?string
+    {
+        return $this->contactTelephone;
+    }
+
+    public function setContactTelephone(string $contactTelephone): self
+    {
+        $this->contactTelephone = $contactTelephone;
+
+        return $this;
+    }
+
+    public function getContactEmail(): ?string
+    {
+        return $this->contactEmail;
+    }
+
+    public function setContactEmail(string $contactEmail): self
+    {
+        $this->contactEmail = $contactEmail;
 
         return $this;
     }
