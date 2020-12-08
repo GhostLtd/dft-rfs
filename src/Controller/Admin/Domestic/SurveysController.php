12167fcfd8498be8eaef40d55ef15b682dafa075
Tom Sykes <tom@tomarq.co.uk>
2020-12-08 16:55:11 +0000

    admin domestic routes

--- src/Controller/Admin/Domestic/SurveysController.php
+++ src/Controller/Admin/Domestic/SurveysController.php
@@ -3,21 +3,41 @@
 namespace App\Controller\Admin\Domestic;
 
 use App\Entity\Domestic\Survey;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class SurveysController
+ * @package App\Controller\Admin\Domestic
+ *
+ * @Route("/csrgt/{type}", name="admin_domestic_", requirements={"type": "gb|ni"})
+ */
 class SurveysController extends AbstractController
 {
     /**
      * @param $type
      * @return Response
-     * @Route("/domestic/surveys/{type}", name="admin_domestic_surveys", requirements={"type": "gb|ni"})
+     * @Route("/surveys", name="surveys")
      */
     public function index($type): Response
     {
         return $this->render('admin/domestic/surveys/index.html.twig', [
             'surveys' => $this->getDoctrine()->getRepository(Survey::class)->findByTypeWithResponseAndVehicle($type === 'ni'),
+        ]);
+    }
+
+    /**
+     * @param $type
+     * @param Survey $survey
+     * @return Response
+     * @Route("/surveys/{{ survey }}", name="surveydetails")
+     */
+    public function viewDetails($type, Survey $survey): Response
+    {
+        return $this->render('admin/domestic/surveys/view.html.twig', [
+            'survey' => $survey,
         ]);
     }
 }
