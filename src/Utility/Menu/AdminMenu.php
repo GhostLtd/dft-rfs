904b42cfdeb773545db3c457ebfe7142ee9d52d1
Mark Allman <mark@tomarq.co.uk>
2020-12-09 16:35:57 +0000

    Admin: Survey creation menu items

--- src/Utility/Menu/AdminMenu.php
+++ src/Utility/Menu/AdminMenu.php
@@ -48,11 +48,13 @@ class AdminMenu implements MenuInterface
             new MenuDivider(),
             new RoleMenuItem('domestic', 'menu.domestic.gb.root', null, [
                 new RoleMenuItem('dashboard', 'menu.domestic.gb.surveys', $this->router->generate('admin_domestic_surveys', ['type' => 'gb']), [], []),
+                new RoleMenuItem('dashboard', 'menu.domestic.gb.survey-add', $this->router->generate('admin_domestic_surveys_add', ['type' => 'gb']), [], []),
                 new RoleMenuItem('dashboard', 'menu.domestic.gb.sub-item-2', null, [], []),
             ]),
             new MenuDivider(),
             new RoleMenuItem('domestic', 'menu.domestic.ni.root', null, [
                 new RoleMenuItem('dashboard', 'menu.domestic.ni.surveys', $this->router->generate('admin_domestic_surveys', ['type' => 'ni']), [], []),
+                new RoleMenuItem('dashboard', 'menu.domestic.ni.survey-add', $this->router->generate('admin_domestic_surveys_add', ['type' => 'ni']), [], []),
                 new RoleMenuItem('dashboard', 'menu.domestic.ni.sub-item-2', null, [], []),
             ]),
             new MenuDivider(),
