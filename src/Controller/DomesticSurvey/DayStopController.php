6b441d31540d1e19d671cbe2e19fea2c94417af6
Mark Allman <mark@tomarq.co.uk>
2020-12-11 14:48:39 +0000

    Fix problem with change routes on Domestic Day Stop

--- src/Controller/DomesticSurvey/DayStopController.php
+++ src/Controller/DomesticSurvey/DayStopController.php
@@ -8,7 +8,6 @@ use App\Entity\Domestic\DayStop;
 use App\Entity\PasscodeUser;
 use App\Workflow\DomesticSurvey\DayStopState;
 use App\Workflow\FormWizardInterface;
-use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,89 +16,65 @@ use Symfony\Component\Workflow\WorkflowInterface;
 
 class DayStopController extends AbstractSessionStateWorkflowController
 {
-    public const ROUTE_NAME = 'app_domesticsurvey_daystop_init';
+    public const START_ROUTE = 'app_domesticsurvey_daystop_start';
+    public const WIZARD_ROUTE = 'app_domesticsurvey_daystop_wizard';
 
     protected $dayNumber;
-    private $legNumber;
+    protected $stopNumber;
+
+    protected $day;
+    protected $stop;
 
     /**
-     * @Route("/domestic-survey/day-{dayNumber}/stop/{state}", name="app_domesticsurvey_daystop_wizard")
+     * @Route(
+     *     "/domestic-survey/day-{dayNumber}/stop-{stopNumber}/start",
+     *     name=self::START_ROUTE,
+     *     requirements={"stageNumber"="\d+|(add)"}
+     * )
+     * @Route(
+     *     "/domestic-survey/day-{dayNumber}/stop-{stopNumber}/{state}",
+     *     name=self::WIZARD_ROUTE,
+     *     requirements={"stageNumber"="\d+|(add)"}
+     * )
      * @Security("is_granted('EDIT', user.getDomesticSurvey())")
-     * @param WorkflowInterface $domesticSurveyDayStopStateMachine
-     * @param Request $request
-     * @param $dayNumber
-     * @param null | string $state
-     * @return Response
-     * @throws Exception
      */
-    public function index(WorkflowInterface $domesticSurveyDayStopStateMachine, Request $request, $dayNumber, $state = null): Response
+    public function init(WorkflowInterface $domesticSurveyDayStopStateMachine, Request $request, $dayNumber, $stopNumber = "add", $state = null): Response
     {
-        $this->dayNumber = intval($dayNumber);
+        $this->stopNumber = $stopNumber;
+
+        /** @var PasscodeUser $user */
+        $user = $this->getUser();
+
+        /** @var Day $day */
+        $this->day = $this->entityManager->getRepository(Day::class)->getBySurveyAndDayNumber($user->getDomesticSurvey(), $dayNumber);
+        $this->dayNumber = $dayNumber;
+
+        if ($stopNumber === 'add') {
+            $this->stop = new DayStop();
+            $this->day->addStop($this->stop);
+        } else {
+            $this->stop = $this->day->getStopByNumber($stopNumber);
+        }
+
         return $this->doWorkflow($domesticSurveyDayStopStateMachine, $request, $state);
     }
 
-    /**
-     * @Route(
-     *     "/domestic-survey/day-{dayNumber}/stop-{legNumber}/start",
-     *     name="app_domesticsurvey_daystop_start",
-     *     requirements={"legNumber"="\d+|(add)"}
-     * )
-     * @Route(
-     *     "/domestic-survey/day-{dayNumber}/stop-{legNumber}/{state}",
-     *     name=self::ROUTE_NAME,
-     *     requirements={"legNumber"="\d+|(add)"}
-     * )
-     * @param WorkflowInterface $domesticSurveyDayStopStateMachine
-     * @param Request $request
-     * @param $dayNumber
-     * @param string $legNumber
-     * @param null $state
-     * @return Response
-     */
-    public function init(WorkflowInterface $domesticSurveyDayStopStateMachine, Request $request, $dayNumber, $legNumber = "add", $state = null)
-    {
-        // set up the forWizard
-        $this->dayNumber = intval($dayNumber);
-        $this->legNumber = $legNumber;
-        $formWizard = $this->getFormWizard();
-
-        // forward to wizard route
-        return $this->redirectToRoute('app_domesticsurvey_daystop_wizard', ['dayNumber' => $dayNumber, 'state' => $state]);
-    }
-
-    /**
-     * @return FormWizardInterface
-     */
     protected function getFormWizard(): FormWizardInterface
     {
-        /** @var PasscodeUser $user */
-        $user = $this->getUser();
-        /** @var Day $day */
-        $day = $this->entityManager->getRepository(Day::class)->getBySurveyAndDayNumber($user->getDomesticSurvey(), $this->dayNumber);
+        /** @var FormWizardInterface $formWizard */
+        $formWizard = $this->session->get($this->getSessionKey(), new DayStopState());
+        $subject = $formWizard->getSubject();
 
-        /** @var DayStopState $formWizard */
-        if ($this->session->has($this->getSessionKey())) {
-            $formWizard = $this->session->get($this->getSessionKey());
-        } else {
-            $formWizard = new DayStopState();
-            // check legNumber (add / number)
-            if ($this->legNumber === 'add') {
-                $day->addStop($dayStop = new DayStop());
-            } else {
-                $dayStop = $day->getStopByNumber($this->legNumber);
-            }
-            $formWizard->setSubject($dayStop);
-            $this->setFormWizard($formWizard);
+        if (!$subject || !$subject instanceof DayStop || $subject->getId() !== $this->stop->getId()) {
+            $subject = $this->stop; // No subject, or we've changed subject
         }
-//        $dayStop = $this->entityManager->getRepository(DayStop::class)->getBySurveyAndDayNumber($user->getDomesticSurvey(), $this->dayNumber);
-//        if (is_null($formWizard->getSubject())) {
-//            $formWizard->setSubject($dayStop);
-//        }
-        $formWizard->getSubject()->setDay($day);
 
-        if ($formWizard->getSubject()->getId()) {
+        $subject->setDay($this->day);
+        $formWizard->setSubject($subject);
+
+        if ($subject->getId()) {
             // ToDo: replace this with our own merge, or make the form wizard store an array of changes until we're ready to flush
-            $formWizard->setSubject($this->entityManager->merge($formWizard->getSubject()));
+            $formWizard->setSubject($this->entityManager->merge($subject));
         }
 
         return $formWizard;
@@ -107,6 +82,6 @@ class DayStopController extends AbstractSessionStateWorkflowController
 
     protected function getRedirectUrl($state): Response
     {
-        return $this->redirectToRoute('app_domesticsurvey_daystop_wizard', ['dayNumber' => $this->dayNumber, 'state' => $state]);
+        return $this->redirectToRoute(self::WIZARD_ROUTE, ['dayNumber' => $this->dayNumber, 'stopNumber' => $this->stopNumber, 'state' => $state]);
     }
 }
