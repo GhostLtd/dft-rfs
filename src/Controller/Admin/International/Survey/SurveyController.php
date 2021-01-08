a2248579f0df363f3318c9c53bc7a5674ec0467b
Mark Allman <mark@tomarq.co.uk>
2021-01-08 16:54:58 +0000

    IRHS: View survey - remaining details

--- src/Controller/Admin/International/Survey/SurveyController.php
+++ src/Controller/Admin/International/Survey/SurveyController.php
@@ -3,20 +3,27 @@
 namespace App\Controller\Admin\International\Survey;
 
 use App\Entity\International\Survey;
+use App\Entity\International\Vehicle;
 use App\ListPage\International\SurveyListPage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/irhs/surveys", name="admin_international_survey_")
+ * @Route("/irhs/surveys")
  */
 class SurveyController extends AbstractController
 {
+    private const ROUTE_PREFIX = 'admin_international_survey_';
+    public const LIST_ROUTE = self::ROUTE_PREFIX.'list';
+    public const VIEW_ROUTE = self::ROUTE_PREFIX.'view';
+    public const VEHICLE_ROUTE = self::ROUTE_PREFIX.'view_vehicle';
+
     /**
-     * @Route("", name="list")
+     * @Route("", name=self::LIST_ROUTE)
      */
     public function list(SurveyListPage $listPage, Request $request): Response
     {
@@ -34,12 +41,27 @@ class SurveyController extends AbstractController
     }
 
     /**
-     * @Route("/view/{survey}", name="view")
+     * @Route("/view/{survey}", name=self::VIEW_ROUTE)
      */
     public function view(Survey $survey): Response
     {
         return $this->render('admin/international/surveys/view.html.twig', [
             'survey' => $survey,
+        ]);
+    }
+
+    /**
+     * @Route("/view/{survey}/vehicle/{vehicle}", name=self::VEHICLE_ROUTE)
+     */
+    public function viewVehicle(Survey $survey, Vehicle $vehicle): Response
+    {
+        if ($vehicle->getSurveyResponse()->getId() !== $survey->getResponse()->getId()) {
+            throw new NotFoundHttpException();
+        }
+
+        return $this->render('admin/international/surveys/view.html.twig', [
+            'survey' => $survey,
+            'vehicle' => $vehicle,
         ]);
     }
 }
