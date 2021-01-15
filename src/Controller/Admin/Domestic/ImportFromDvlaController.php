1d0734d4e5f837be14ff232b91f2c10288f0f3b7
Tom Sykes <tom@tomarq.co.uk>
2021-01-15 13:34:26 +0000

    - fix domestic edit initial details - clean up domestic admin urls

--- src/Controller/Admin/Domestic/ImportFromDvlaController.php
+++ src/Controller/Admin/Domestic/ImportFromDvlaController.php
@@ -12,7 +12,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
