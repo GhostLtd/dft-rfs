c67a855d492b5daec493e156669cc77d3187d8e9
Mark Allman <mark@tomarq.co.uk>
2021-01-19 10:50:22 +0000

    Dom: Admin - make route formats consistent with IRHS

--- src/Controller/Admin/Domestic/SurveyController.php
+++ src/Controller/Admin/Domestic/SurveyController.php
@@ -5,22 +5,30 @@ namespace App\Controller\Admin\Domestic;
 use App\Entity\BlameLog\BlameLog;
 use App\Entity\Domestic\Survey;
 use App\ListPage\Domestic\SurveyListPage;
-use App\Repository\BlameLogRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/csrgt", name="admin_domestic_")
+ * @Route("/csrgt/surveys")
  */
-class SurveysController extends AbstractController
+class SurveyController extends AbstractController
 {
+    private const ROUTE_PREFIX = 'admin_domestic_survey_';
+    public const DELETE_ROUTE = self::ROUTE_PREFIX.'delete';
+    public const LIST_ROUTE = self::ROUTE_PREFIX.'list';
+    public const LOGS_ROUTE = self::ROUTE_PREFIX.'logs';
+    public const VIEW_ROUTE = self::ROUTE_PREFIX.'view';
+
+    public const EDIT_INITIAL_ROUTE = self::ROUTE_PREFIX.'initial_edit';
+    public const EDIT_BUSINESS_ROUTE = self::ROUTE_PREFIX.'business_edit';
+
     /**
-     * @Route("/surveys/{type}", name="surveys", requirements={"type": "gb|ni"})
+     * @Route("/{type}", requirements={"type": "gb|ni"}, name=self::LIST_ROUTE)
      */
     public function list(SurveyListPage $listPage, Request $request, string $type): Response
     {
@@ -40,7 +48,8 @@ class SurveysController extends AbstractController
     }
 
     /**
-     * @Route("/survey/view/{survey}", name="surveydetails")
+     * @Route("/{surveyId}", name=self::VIEW_ROUTE)
+     * @Entity("survey", expr="repository.find(surveyId)")
      */
     public function viewDetails(Survey $survey): Response
     {
@@ -50,10 +59,8 @@ class SurveysController extends AbstractController
     }
 
     /**
-     * @param EntityManagerInterface $blameLogEntityManager
-     * @param Survey $survey
-     * @return Response
-     * @Route("/survey/view/{survey}/audit-log", name="surveylogs")
+     * @Route("/{surveyId}/audit-log", name=self::LOGS_ROUTE)
+     * @Entity("survey", expr="repository.find(surveyId)")
      */
     public function viewLog(EntityManagerInterface $blameLogEntityManager, Survey $survey): Response
     {
--- src/Utility/Menu/AdminMenu.php
+++ src/Utility/Menu/AdminMenu.php
@@ -2,6 +2,8 @@
 
 namespace App\Utility\Menu;
 
+use App\Controller\Admin\Domestic\SurveyController as DomSurveyController;
+use App\Controller\Admin\International\SurveyController as IntSurveyController;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Security;
@@ -40,15 +42,15 @@ class AdminMenu implements MenuInterface
             new RoleMenuItem('dashboard', 'menu.dashboard', $this->router->generate('admin_index'), [], []),
             new MenuDivider(),
             new RoleMenuItem('domestic', 'menu.domestic.root', null, [
-                new RoleMenuItem('surveys-gb', 'menu.domestic.surveys-gb', $this->router->generate('admin_domestic_surveys', ['type' => 'gb']), [], []),
-                new RoleMenuItem('surveys-ni', 'menu.domestic.surveys-ni', $this->router->generate('admin_domestic_surveys', ['type' => 'ni']), [], []),
+                new RoleMenuItem('surveys-gb', 'menu.domestic.surveys-gb', $this->router->generate(DomSurveyController::LIST_ROUTE, ['type' => 'gb']), [], []),
+                new RoleMenuItem('surveys-ni', 'menu.domestic.surveys-ni', $this->router->generate(DomSurveyController::LIST_ROUTE, ['type' => 'ni']), [], []),
                 new RoleMenuItem('dvla-import', 'menu.domestic.dvla-import', $this->router->generate('admin_domestic_importdvla_index'), [], []),
                 new RoleMenuItem('add-survey', 'menu.domestic.survey-add', $this->router->generate('admin_domestic_survey_add'), [], []),
             ]),
             new MenuDivider(),
 
             new RoleMenuItem('international', 'menu.international.root', null, [
-                new RoleMenuItem('survey', 'menu.international.surveys', $this->router->generate('admin_international_survey_list'), [], []),
+                new RoleMenuItem('survey', 'menu.international.surveys', $this->router->generate(IntSurveyController::LIST_ROUTE), [], []),
                 new RoleMenuItem('add-survey', 'menu.international.survey-add', $this->router->generate('admin_international_survey_add'), [], []),
             ]),
             new MenuDivider(),
