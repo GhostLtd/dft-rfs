6929aca607ac3c8201b14d97dd8314d02a09e988
Mark Allman <mark@tomarq.co.uk>
2021-01-27 14:15:08 +0000

    Dom: Admin - day-stop add

--- src/Controller/Admin/Domestic/DayStopController.php
+++ src/Controller/Admin/Domestic/DayStopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin\Domestic;
 
+use App\Entity\Domestic\Day;
 use App\Entity\Domestic\DayStop;
 use App\Form\Admin\DomesticSurvey\DayStopDeleteType;
 use App\Form\Admin\DomesticSurvey\Edit\DayStopType;
@@ -13,26 +14,63 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/csrgt/day-stops")
+ * @Route("")
  */
 class DayStopController extends AbstractController
 {
     private const ROUTE_PREFIX = 'admin_domestic_daystop_';
+    public const ADD_ROUTE = self::ROUTE_PREFIX . 'add';
     public const DELETE_ROUTE = self::ROUTE_PREFIX . 'delete';
     public const EDIT_ROUTE = self::ROUTE_PREFIX . 'edit';
 
+    protected EntityManagerInterface $entityManager;
+    protected RequestStack $requestStack;
+
+    public function __construct(EntityManagerInterface $entityManager, RequestStack $requestStack)
+    {
+        $this->entityManager = $entityManager;
+        $this->requestStack = $requestStack;
+    }
+
     /**
-     * @Route("/{stopId}", name=self::EDIT_ROUTE)
+     * @Route("/days/{dayId}/add-day-stop", name=self::ADD_ROUTE)
+     * @Entity("day", expr="repository.find(dayId)")
+     */
+    public function add(Day $day): Response
+    {
+        if ($day->getSummary() !== null) {
+            throw new BadRequestHttpException();
+        }
+
+        $stop = (new DayStop());
+        $day->addStop($stop);
+
+        $this->entityManager->persist($stop);
+
+        return $this->handleRequest($stop, "admin/domestic/stop/add.html.twig", ['is_add_form' => true]);
+    }
+
+    /**
+     * @Route("/csrgt/day-stops/{stopId}", name=self::EDIT_ROUTE)
      * @Entity("stop", expr="repository.find(stopId)")
      */
-    public function edit(DayStop $stop, Request $request, EntityManagerInterface $entityManager)
+    public function edit(DayStop $stop)
+    {
+        return $this->handleRequest($stop, "admin/domestic/stop/edit.html.twig");
+    }
+
+    protected function handleRequest(DayStop $stop, string $template, array $formOptions = [])
     {
         $survey = $stop->getDay()->getResponse()->getSurvey();
 
-        $form = $this->createForm(DayStopType::class, $stop);
+        $form = $this->createForm(DayStopType::class, $stop, $formOptions);
+        $request = $this->requestStack->getCurrentRequest();
 
         if ($request->getMethod() === Request::METHOD_POST) {
             $form->handleRequest($request);
@@ -44,22 +82,22 @@ class DayStopController extends AbstractController
             }
 
             if ($form->isValid()) {
-                $entityManager->flush();
+                $this->entityManager->flush();
                 return $redirectResponse;
             }
         }
 
-        return $this->render("admin/domestic/stop/edit.html.twig", [
+        return $this->render($template, [
             'survey' => $survey,
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/{stopId}/delete", name=self::DELETE_ROUTE)
+     * @Route("/csrgt/day-stops/{stopId}/delete", name=self::DELETE_ROUTE)
      * @Entity("stop", expr="repository.find(stopId)")
      */
-    public function delete(DayStop $stop, Request $request, DeleteHelper $deleteHelper)
+    public function delete(DayStop $stop, DeleteHelper $deleteHelper)
     {
         $form = $this->createForm(DayStopDeleteType::class);
 
@@ -69,6 +107,7 @@ class DayStopController extends AbstractController
         $redirectUrl = $this->generateUrl(SurveyController::VIEW_ROUTE, ['surveyId' => $survey->getId()])."#{$day->getId()}";
 
         $translationPrefix = 'domestic.day-stop-delete';
+        $request = $this->requestStack->getCurrentRequest();
         if ($request->getMethod() === Request::METHOD_POST) {
             $form->handleRequest($request);
 
