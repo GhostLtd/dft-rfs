ec8789fb629ce4eb71db95cee26fa0ed6275eb4c
Mark Allman <mark@tomarq.co.uk>
2021-01-27 12:07:12 +0000

    Dom: Admin - day-summary delete

--- src/Controller/Admin/Domestic/DaySummaryController.php
+++ src/Controller/Admin/Domestic/DaySummaryController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Admin\Domestic;
 
 use App\Entity\Domestic\DaySummary;
+use App\Form\Admin\DomesticSurvey\DaySummaryDeleteType;
 use App\Form\Admin\DomesticSurvey\Edit\DaySummaryType;
+use App\Utility\Domestic\DeleteHelper;
 use Doctrine\ORM\EntityManagerInterface;
+use Ghost\GovUkFrontendBundle\Model\NotificationBanner;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\SubmitButton;
@@ -56,8 +59,38 @@ class DaySummaryController extends AbstractController
      * @Route("/{summaryId}/delete", name=self::DELETE_ROUTE)
      * @Entity("summary", expr="repository.find(summaryId)")
      */
-    public function delete(DaySummary $summary)
+    public function delete(DaySummary $summary, Request $request, DeleteHelper $deleteHelper)
     {
+        $form = $this->createForm(DaySummaryDeleteType::class);
 
+        $day = $summary->getDay();
+        $survey = $day->getResponse()->getSurvey();
+
+        $redirectUrl = $this->generateUrl(SurveyController::VIEW_ROUTE, ['surveyId' => $survey->getId()])."#{$day->getId()}";
+
+        $translationPrefix = 'domestic.day-summary-delete';
+        if ($request->getMethod() === Request::METHOD_POST) {
+            $form->handleRequest($request);
+
+            $delete = $form->get('delete');
+
+            $notificationPrefix = "{$translationPrefix}.notification";
+            if ($delete instanceof SubmitButton && $delete->isClicked()) {
+                $deleteHelper->deleteDaySummary($summary);
+
+                $this->addFlash(NotificationBanner::FLASH_BAG_TYPE, $deleteHelper->getDeletedNotification($notificationPrefix));
+                return new RedirectResponse($redirectUrl);
+            } else {
+                $this->addFlash(NotificationBanner::FLASH_BAG_TYPE, $deleteHelper->getCancelledNotification($notificationPrefix));
+                return new RedirectResponse($redirectUrl);
+            }
+        }
+
+        return $this->render('admin/domestic/summary/delete.html.twig', [
+            'stop' => $summary,
+            'survey' => $survey,
+            'form' => $form->createView(),
+            'translation_prefix' => $translationPrefix,
+        ]);
     }
 }
--- src/Utility/Domestic/DeleteHelper.php
+++ src/Utility/Domestic/DeleteHelper.php
@@ -3,6 +3,7 @@
 namespace App\Utility\Domestic;
 
 use App\Entity\Domestic\DayStop;
+use App\Entity\Domestic\DaySummary;
 use App\Utility\AbstractDeleteHelper;
 
 class DeleteHelper extends AbstractDeleteHelper
@@ -10,6 +11,12 @@ class DeleteHelper extends AbstractDeleteHelper
     public function deleteDayStop(DayStop $stop, bool $flush=true)
     {
         $this->entityManager->remove($stop);
+        $flush && $this->entityManager->flush();
+    }
+
+    public function deleteDaySummary(DaySummary $summary, bool $flush=true)
+    {
+        $this->entityManager->remove($summary);
         $flush && $this->entityManager->flush();
     }
 }
